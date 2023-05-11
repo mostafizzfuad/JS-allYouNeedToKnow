babea08a70cd6c41df4a4bb019b1e225b707e431
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-05-11 18:25:34 +0600

    update code

---
+++ 01_hello.js
@@ -0,0 +1,12 @@
+// what is programming ?
+// => Programming is a set of instruction which our computer can read, understand and produce and output.
+
+
+console.log('Hello world')
+console.log('I love Bangladesh')
+console.log(45)
+console.log(60.789)
+
+console.log('My favorite no: ' + 9) // string + number = string
+console.log('9' + 9) // string + number = string
+console.log(9 + 9) // number + number = number
---
+++ 02_variable.js
@@ -0,0 +1,12 @@
+// variable : variable is a container which can Store Data.
+
+var myName = "Mostafizur Rahman Fuad"
+var myAge = 25
+
+console.log(myName)
+console.log(myAge)
+
+console.log(myName + ' knows javascript.')
+console.log('His age is ' + myAge)
+console.log(myName + ' is creating a Javascript Course for All.')
+console.log('But his age is only ' + myAge)
