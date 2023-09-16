c52ccd197b93c7a8ef8b1ddbd4c161d7764706c4
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-09-17 00:56:32 +0600

    update code

--- 22_forEach.js
+++ 22_forEach.js
@@ -1,3 +1,15 @@
+// forEach() : ai function er kaj hocche 1 ta array ke first to last traverse kora.
+
+// example : array traverse using functional programming : forEach()
+var arr = [1, 2, 3, 4, 5]
+
+arr.forEach(function(value, index, arr) {
+    console.log(value , index , arr)
+})
+
+
+
+// example : sum of all array elements using forEach()
 var arr = [1, 2, 3, 4, 5]
 
 var sum = 0
@@ -10,7 +22,7 @@ console.log(sum)
 
 
 
-
+/************ implementation forEach() *************/
 var arr = [1, 2, 3, 4, 5]
 
 function forEach(arr, cb) {
@@ -20,15 +32,22 @@ function forEach(arr, cb) {
     }
 }
 
-var sum = 0
+
+// example : array traverse
 forEach(arr, function(value, index, arr) {
     console.log(value, index, arr)
+})
+
+
+// example : sum of all array elements
+var sum = 0
+forEach(arr, function(value, index, arr) {
     sum += value
 })
-console.log(sum) 
+console.log(sum)
 
 
-// add 5 in every single element
+// example : add 5 in every single element
 forEach(arr, function(value, index, arr) {
     arr[index] = value + 5
 })
